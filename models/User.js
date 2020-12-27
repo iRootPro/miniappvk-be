d666a7ecd50c89159439134b4a9a97dbd6b44e25
Alexander Neupokoev <admin@armavir.ru>
2020-12-27 12:16:07 +0300

    double reply fix

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const User = new Schema({
     coin: {type: Number, default: 0},
     user_vk_id: {type: Number, required: true},
     message: [{type: String}],
-    comment_id: {type: Number, default: 0, required: true}
+    comment_id: {type: Number, default: 0}
 })
 
 module.exports = model('User', User)
