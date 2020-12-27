7e358b39bc9580a7e302ef442fc2adb8bf9ad189
Alexander Neupokoev <admin@armavir.ru>
2020-12-27 08:09:00 +0300

    add db

--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@ const {Schema, model, ObjectId} = require("mongoose")
 
 const User = new Schema({
     coin: {type: Number, default: 0},
-    user_vk_id: {type: String, required: true}
+    user_vk_id: {type: String, required: true},
+    message: [{type: String}]
 })
 
 module.exports = model('User', User)
