b36327e7e510342a89e5b7997f1917cd40ca3028
Alexander Neupokoev <admin@armavir.ru>
2020-12-27 08:31:55 +0300

    testing fb

--- routes/root.routes.js
+++ routes/root.routes.js
@@ -3,11 +3,11 @@ const User = require('./../models/User')
 const router = Router()
 
 router.post('/', async (req, res) => {
-        console.log(req)
-        if (req.body.type === 'wall_reply_new') {
-            const existUser = await User.findOne({user_vk_id: req.body.object.from_id})
-            console.log('### id:', req.body.object.from_id)
+        if (req.body.type !== 'wall_reply_new') {
+            return
         }
+        const existUser = await User.findOne({user_vk_id: req.body.object.from_id})
+        console.log('### user:', existUser)
         // registration Server on vk
         // res.status(200).send('91a4d37c')
     }
