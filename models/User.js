47e7a552df00d21870c2b04c1c39d3a63e88385c
Alexander Neupokoev <admin@armavir.ru>
2020-12-27 17:02:21 +0300

    recieve data info user;

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const User = new Schema({
     coin: {type: Number, default: 0},
     user_vk_id: {type: Number, required: true},
     message: [{type: String}],
-    comment_id: {type: Number, default: 0}
+    comment_id: {type: Number, default: 0},
     first_name: {type: String},
     last_name: {type: String},
     avatar: {type: String},
