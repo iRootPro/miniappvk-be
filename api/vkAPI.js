5e8c73bba95cd43b46a67364305c730ddd0e9a04
Alexander Neupokoev <admin@armavir.ru>
2020-12-27 16:52:12 +0300

    recieve data info user;

--- api/vkAPI.js
+++ api/vkAPI.js
@@ -8,7 +8,7 @@ const instance = axios.create({
 
 const vkAPI = {
     async getUserInfo(id) {
-        return instance.get(`users.get?user_id=${id}&v=5.52`)
+        return instance.get(`users.get?user_id=${id}&access_token=d4faa8e9d4faa8e9d4faa8e993d48f0037dd4fad4faa8e98b1da2eb2cb357364c38d2ea&v=5.52`)
     }
 }
 
