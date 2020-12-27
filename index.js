f2688c5dac26706e0f4ec65cbf605502e5bffbb9
Alexander Neupokoev <admin@armavir.ru>
2020-12-27 10:43:40 +0300

    add cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const https = require('https')
 const config = require('config')
 const mongoose = require('mongoose')
@@ -10,6 +11,7 @@ const PORT = config.get('serverPort')
 const dbURL = config.get('dbURL')
 
 app.use(express.json())
+app.use(cors())
 app.use('', rootRouter)
 
 const httpsOptions = {
